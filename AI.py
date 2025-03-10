f1e9a1adc816d4c59c3883b1a7f61fe192a73829
shadowforce78 <planque.adam@gmail.com>
2025-03-10 13:34:52 +0100

    Enhance JSON formatting in format_edt_ia function for improved data readability

--- AI.py
+++ AI.py
@@ -8,10 +8,23 @@ gemini_api_key = os.getenv("GEMINI")
 
 
 def format_edt_ia(datajson):
+
+    format = """
+        "Date": "",
+        "Heure": "",
+        "Matière": ,
+        "Personnel": ,
+        "Groupe": "",
+        "Salle": "",
+        "Catégorie d’événement": "",
+        "Remarques" : ""
+        """
+
     prompt = (
         "Reorganise les données suivantes en un tableau lisible :"
         + datajson
-        + " , je ne veux que du json brut"
+        + " , je ne veux que du json brut au format suivant :"
+        + format
     )
 
     client = genai.Client(api_key=gemini_api_key)
@@ -20,5 +33,5 @@ def format_edt_ia(datajson):
         model="gemini-2.0-flash",
         contents=[prompt],
     )
-    
+
     return response.text
