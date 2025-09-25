850c5ed0015d55668b47b54e1e6227c101e9f415
shadowforce78 <planque.adam@gmail.com>
2025-09-25 14:20:35 +0200

    SUPPRIMER: retirer l'origine CORS pour le port 5173

--- config.py
+++ config.py
@@ -18,7 +18,6 @@ class Settings:
         "http://localhost:8080",  # Vue
         "http://localhost:4200",  # Angular
         "http://127.0.0.1:5500", # Live Server
-        "http://127.0.0.1:5173",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:4200",
         "http://localhost:8081" # Expo React Native
