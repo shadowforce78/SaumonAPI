01ca5c77e3691516ab0fb6694805e213a9d2ac6d
shadowforce78 <planque.adam@gmail.com>
2025-09-25 11:28:53 +0200

    AJOUT: ajouter l'origine CORS pour UVSQ dans la configuration

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Settings:
         # Environnement de développement
         "https://localhost",  # Capacitor
         "https://scan.saumondeluxe.com", # Scan
+        "https://uvsq.saumondeluxe.com", # UVSQ
         "http://localhost:3000",  # React
         "http://localhost:5173",  # Vite
         "http://localhost:8080",  # Vue
